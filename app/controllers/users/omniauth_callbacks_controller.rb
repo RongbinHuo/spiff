7f6975b7cddca47700d0082790e043197f98e1b0
Rongbin <rongbin@amg.tv>
2017-04-11 15:46:32 -0400

    final version to use google auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,10 +3,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      puts "aaaaaaaa login #{@user.inspect}"
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      Authentication.create(@user,true)
+      redirect_to root_path, notice: "Welcome back #{@user.name}"
       # sign_in_and_redirect @user, :event => :authentication
-      redirect_back_or_default root_url @user, :event => :authentication
     else
       session["devise.google_data"] = request.env["omniauth.auth"].except(:extra) #Removing extra as it can overflow some session stores
       redirect_to root_path, alert: @user.errors.full_messages.join("\n")
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '814629565728-vufnipordi2bjaujd0c2ht89olc2ki3v.apps.googleusercontent.com', '9dPeWPz8dg_Rc5BLxbZir7wJ',
-    {
-      :name => "google",
-      # :scope => "email, profile, plus.me, http://gdata.youtube.com",
-      :prompt => "select_account",
-      :image_aspect_ratio => "square",
-      :image_size => 50
-    }
+  provider :google_oauth2, '814629565728-vufnipordi2bjaujd0c2ht89olc2ki3v.apps.googleusercontent.com', '9dPeWPz8dg_Rc5BLxbZir7wJ'
 end
